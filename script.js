63926fa7fb7b01ed21be45c2039800e2e7be65bc
nilj07 <111432313+nilj07@users.noreply.github.com>
2025-01-21 15:57:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ button.addEventListener("click", ()=>{
         
         const msg = value;
         localStorage.setItem('message', msg)
-        img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${msg}`
+        img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://resultqr.netlify.app/`
 
     }
     
